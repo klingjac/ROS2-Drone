be31a3e12e7323daa40657f6ee93f71990c1e8e8
Jacob <klingjac@umich.edu>
2023-12-06 21:04:40 -0500

    Add PX4 sub-projects as regular directories

---
+++ build/px4_ros_com/CMakeFiles/drone_position_plotter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drone_position_plotter.dir/src/examples/traj_plot/drone_position_plotter.cpp.o"
+  "CMakeFiles/drone_position_plotter.dir/src/examples/traj_plot/drone_position_plotter.cpp.o.d"
+  "drone_position_plotter"
+  "drone_position_plotter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drone_position_plotter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/px4_offboard/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/klingjac/ws_offboard_control/src/install/px4_offboard'
